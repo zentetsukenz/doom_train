b3653bd862f710ae8bbc6883b4f40bf194637528
Wiwatta  Mongkhonchit <zentetsukenz@gmail.com>
2017-06-19 02:27:09 +0700

    Schedule to run currency pairings everyday

--- lib/doom_train/station/bx.ex
+++ lib/doom_train/station/bx.ex
@@ -13,18 +13,22 @@ defmodule DoomTrain.Station.Bx do
 
   def init(:ok) do
     schedule_currency_pairing()
-    {:ok, %{}}
+    {:ok, %{currency_pairs: nil}}
   end
 
   def handle_info(:currency_pairing, state) do
-    Bx.currency_pairings() |> IO.inspect
+    currency_pairs = Bx.currency_pairings().body
 
     schedule_currency_pairing()
-
-    {:noreply, state}
+    {:noreply, Map.put(state, :currency_pairs, currency_pairs)}
   end
 
+  # Scheduling
+
   defp schedule_currency_pairing do
-    Process.send_after(self(), :currency_pairing, 60 * 1000)
+    #  Schedule it to run again next 24 hrs
+    #  TODO: Next running time must be calibrate to run at a specific time
+    #  instead of let it drifts away everyday.
+    Process.send_after(self(), :currency_pairing, 24 * 60 * 60 * 1000)
   end
 end
